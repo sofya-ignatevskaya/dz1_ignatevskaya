90d146a9d65dbc452f3d7039497b14eee4974d54
Sofya Ignatevskaya <57006805+sofya-ignatevskaya@users.noreply.github.com>
2021-03-05 20:45:01 +0300

    second commit

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,15 @@
+import model.Kotik;
+
 public class Application {
+    public static void main(String[] args) {
+        Kotik kotik1 = new Kotik(1, 2, "Tom", "Meooow",100);
+        Kotik kotik2 = new Kotik();
+        kotik2.setKotik(2, 3, "Murka", "Mur-Mur", 80);
+        kotik1.liveAnotherDay();
+        System.out.println("\nName is " + kotik1.getName() + ". Weight is " + kotik1.getWeight() + "\n");
+        System.out.println(kotik1.equals(kotik2));
+        System.out.println();
+        Kotik.getCount();
+    }
+
 }
--- src/model/Kotik.java
+++ src/model/Kotik.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Kotik {
     private int prettiness;
     private int weight;
@@ -7,7 +10,10 @@ public class Kotik {
     private String meow;
     private int satiety;
 
+    public static int count;
+
     public Kotik() {
+        count++;
     }
 
     public Kotik(int prettiness, int weight, String name, String meow, int satiety) {
@@ -16,11 +22,13 @@ public class Kotik {
         this.name = name;
         this.meow = meow;
         this.satiety = satiety;
+        count++;
     }
-    public void setKotik(int prettiness, int weight, String name, String meow, int satiety){
+
+    public void setKotik(int prettiness, int weight, String name, String meow, int satiety) {
         this.prettiness = prettiness;
         this.weight = weight;
-        this.name=name;
+        this.name = name;
         this.meow = meow;
         this.satiety = satiety;
     }
@@ -41,34 +49,108 @@ public class Kotik {
         return meow;
     }
 
-    public boolean sleep(){
-        if (satiety > 0){
-            System.out.println(this.getName() + " is sleeping");
-            satiety += 30;
+    public static void getCount(){
+        System.out.println("Quantity of object is " + count);
+    }
+
+    public boolean sleep() {
+        if (satiety > 0) {
+            satiety -= 5;
+            System.out.println(this.name + " is sleeping. Satiety is " + this.satiety);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public boolean play(){
-        if (satiety > 0){
-            System.out.println(this.getName()+ " is playing");
+    public boolean play() {
+        if (satiety > 0) {
             satiety -= 20;
+            System.out.println(this.name + " is playing. Satiety is " + this.satiety);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public boolean sayMeow(){
-        if (satiety > 0){
-            System.out.println(this.getName()+ " is sleeping");
+    public boolean sayMeow() {
+        if (satiety > 0) {
             satiety -= 10;
+            System.out.println(this.name + " is saying " + this.meow + ". Satiety is " + this.satiety );
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
+
+    public boolean chaseMouse() {
+        if (satiety > 0) {
+            satiety -= 50;
+            System.out.println(this.name + " is chasing a mouse. Satiety is " + this.satiety );
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean wash() {
+        if (satiety > 0) {
+            satiety -= 15;
+            System.out.println(this.name + " is washing yourself. Satiety is " + this.satiety);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void eat(int satiety) {
+        this.satiety += satiety;
+        System.out.println(this.name + " is eating. Satiety is " + this.satiety);
+    }
+
+    public void eat(int satiety, String nameFood) {
+        this.satiety += satiety;
+        System.out.println(this.name + " is eating " + nameFood + ". Satiety is " + this.satiety);
+    }
+
+    public void eat() {
+        eat(100, "Whiskas");
+    }
+
+    public void liveAnotherDay() {
+        for (int i = 0; i < 24; i++) {
+           // System.out.println(i);
+            if (this.satiety > 0) {
+                Random random = new Random();
+                int r = random.nextInt(5) + 1;
+                switch (r) {
+                    case 1:
+                        sleep();
+                        break;
+                    case 2:
+                        play();
+                        break;
+                    case 3:
+                        sayMeow();
+                        break;
+                    case 4:
+                        chaseMouse();
+                        break;
+                    case 5:
+                        wash();
+                        break;
+                }
+            } else {
+                System.out.print("Котику пора покушать! ");
+                eat();
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Kotik otherKotik = (Kotik)object;
+        return this.meow.equals(otherKotik.meow);
     }
 
 }
